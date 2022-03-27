e13ce8cda57c6cb1ced9e84cefa7b236f17495d9
Yeonju Kim <ddangjjong@gmail.com>
2022-03-27 18:20:09 +0900

    update 2588.py

--- BAEKJOON/Bronze IV/2588.py
+++ BAEKJOON/Bronze IV/2588.py
@@ -1,7 +1,19 @@
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
 
-print(a * (b%10))
-print(a * ((b%100)//10))
-print(a * ((b%1000)//100))
-print(a*b)
+
+# #방법 1: 나머지 이용
+# print(a * (b%10))
+# print(a * ((b%100)//10))
+# print(a * ((b%1000)//100))
+# print(a*b)
+
+
+#방법 2: range(시작,끝, 순서)이용해 역순으로 출력
+a = int(input())
+b = input()
+
+for i in range(len(b),0,-1):
+    print(a*int(b[i-1]))
+
+print(a*int(b))
